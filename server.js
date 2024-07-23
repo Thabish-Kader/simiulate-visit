aee1512c8b674e0103e46874277971623319d84d
Thabish <thabish@Chips-and-Dips.local>
2024-07-23 16:48:56 +0530

    simulate
    Signed-off-by: Thabish <thabish@Chips-and-Dips.local>

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+
+const app = express();
+
+let browser;
+
+async function initializeBrowser() {
+  browser = await puppeteer.launch({ headless: false });
+}
+async function simulateVisits() {
+  try {
+    const page = await browser.newPage();
+    await page.goto("https://google.com/", { waitUntil: "networkidle0" });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+app.get("/simulation", async (req, res) => {
+  try {
+    await simulateVisits();
+    res.send("Simulation started");
+  } catch (error) {
+    res.status(500).send("Error: " + error);
+  }
+});
+
+app.listen(3000, async () => {
+  await initializeBrowser();
+  console.log("Server started on port 3000");
+});
